cd17a1ce4349c9a8deaa16f67d63ba86577928ae
aekc <972424611@qq.com>
2018-09-24 17:54:37 +0800

    complete RemoveNthNodeFromEndOfList

--- src/com/aekc/leetcode/RemoveNthNodeFromEndOfList.java
+++ src/com/aekc/leetcode/RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,45 @@
 package com.aekc.leetcode;
 
 public class RemoveNthNodeFromEndOfList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int k = 0;
+        ListNode listNode = head;
+        while(listNode != null) {
+            ++k;
+            listNode = listNode.next;
+        }
+        int p = 0;
+        ListNode front = null;
+        ListNode behind = head;
+        while(p++ != k - n && behind.next != null) {
+            front = behind;
+            behind = behind.next;
+        }
+        if(front != null) {
+            front.next = behind.next;
+        } else {
+            head = head.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        RemoveNthNodeFromEndOfList removeNthNodeFromEndOfList = new RemoveNthNodeFromEndOfList();
+        ListNode head = removeNthNodeFromEndOfList.new ListNode(1);
+
+        head = removeNthNodeFromEndOfList.removeNthFromEnd(head, 1);
+        while(head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
 }
