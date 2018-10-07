d1ce21e7a4dec5ca2797e09bf29bb6ff6bd3c8f0
twilight <972424611@qq.com>
2018-10-07 09:11:14 +0800

    Complete ThreeSumClosest

--- src/com/aekc/leetcode/LongestPalindromicSubstring.java
+++ src/com/aekc/leetcode/LongestPalindromicSubstring.java
@@ -9,7 +9,7 @@ import java.util.Set;
  */
 public class LongestPalindromicSubstring {
 
-    public String longestPalindrome(String s) {
+    /*public String longestPalindrome(String s) {
         if(s.length() < 2) {
             return s;
         }
@@ -41,13 +41,52 @@ public class LongestPalindromicSubstring {
             }
         }
         return palindromic;
+    }*/
+
+    public String longestPalindrome(String s) {
+        if(s.length() < 2) {
+            return s;
+        }
+        String palindromic = "";
+        int j, k;
+        for(int i = 1; i < s.length(); i++) {
+            if(s.length() - i <= palindromic.length() / 2) {
+                break;
+            }
+            int sum1 = 1, sum2 = 0;
+            boolean flag1 = true, flag2 = true;
+            for(j = i - 1, k = i; j >= 0 && k < s.length(); j--, k++) {
+                if(flag1 && k + 1 < s.length() && s.charAt(j) == s.charAt(k + 1)) {
+                    sum1 = sum1 + 2;
+                } else {
+                    flag1 = false;
+                }
+
+                if(flag2 && s.charAt(j) == s.charAt(k)) {
+                    sum2 = sum2 + 2;
+                } else {
+                    flag2 = false;
+                }
+
+                if(!flag1 && !flag2) {
+                    break;
+                }
+            }
+            if(sum2 > palindromic.length()) {
+                palindromic = s.substring(j + 1, k);
+            }
+            if(sum1 > palindromic.length()) {
+                palindromic = s.substring(j + 1, k + 1);
+            }
+        }
+        return palindromic;
     }
 
 
     public static void main(String[] args) {
         LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
-        //String s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy";
-        String s = "bb";
+       //String s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy";
+       String s = "bb";
         s = longestPalindromicSubstring.longestPalindrome(s);
         System.out.println(s);
     }
--- src/com/aekc/leetcode/ThreeSumClosest.java
+++ src/com/aekc/leetcode/ThreeSumClosest.java
@@ -1,13 +1,37 @@
 package com.aekc.leetcode;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
 
     public int threeSumClosest(int[] nums, int target) {
-
-        return 0;
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        if(target < 0) {
+            min = min + target;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            while(j < k) {
+                if(Math.abs(nums[i] + nums[j] + nums[k] - target) < Math.abs(min - target)) {
+                    min = nums[i] + nums[j] + nums[k];
+                } else {
+                    if(nums[i] + nums[j] + nums[k] < target) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+        }
+        return min;
     }
 
     public static void main(String[] args) {
-
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        System.out.println(threeSumClosest.threeSumClosest(new int[] {0, 2, 1, -3}, 1));
     }
 }
