cd949b61231d20c8b4ce12e4f0a7daeba3c3cd42
aekc <972424611@qq.com>
2018-11-10 14:00:35 +0800

    complete Noi2727

--- src/com/aekc/noi/Noi2727.java
+++ src/com/aekc/noi/Noi2727.java
@@ -1,6 +1,7 @@
 package com.aekc.noi;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,9 +11,7 @@ public class Noi2727 {
 
     private static int[][] minCoordinate;
 
-    private static boolean[][] flag;
-
-    private static final int MAX_VALUE = 1000000;
+    private static int ans = 0;
 
     private static int n;
 
@@ -22,39 +21,81 @@ public class Noi2727 {
 
     private static int p;
 
-    public static int dp(int i, int j) {
-        if(minCoordinate[i][j] != 0) {
-            return minCoordinate[i][j];
-        }
-        if(coordinate[i][j] == '#') {
-            return MAX_VALUE;
-        }
+    public static void dfs(int i, int j) {
+        int t = minCoordinate[i][j];
         if(i == q && j == p) {
-            return 0;
+            ans = t;
         }
-        flag[i][j] = true;
-        int up = MAX_VALUE;
-        int down = MAX_VALUE;
-        int left = MAX_VALUE;
-        int right = MAX_VALUE;
-        if(i > 0 && !flag[i - 1][j]) {
-            up = 1 + dp(i - 1, j);
+        t++;
+        //不超界限&&不是怪物&&更优
+        if(i > 0 && coordinate[i - 1][j] != '#' && minCoordinate[i - 1][j] > t) {
+            minCoordinate[i - 1][j] = t;
+            dfs(i - 1, j);
         }
-        if(i < n - 1 && !flag[i + 1][j]) {
-            down = 1 + dp(i + 1, j);
+        if(i < n - 1 && coordinate[i + 1][j] != '#' && minCoordinate[i + 1][j] > t) {
+            minCoordinate[i + 1][j] = t;
+            dfs(i + 1, j);
         }
-        if(j > 0 && !flag[i][j - 1]) {
-            left = 1 + dp(i, j - 1);
+        if(j > 0 && coordinate[i][j - 1] != '#' && minCoordinate[i][j - 1] > t) {
+            minCoordinate[i][j - 1] = t;
+            dfs(i, j - 1);
         }
-        if(j < m - 1 && !flag[i][j + 1]) {
-            right = 1 + dp(i, j + 1);
+        if(j < m - 1 && coordinate[i][j + 1] != '#' && minCoordinate[i][j + 1] > t) {
+            minCoordinate[i][j + 1] = t;
+            dfs(i, j + 1);
         }
-        minCoordinate[i][j] = Math.min(up, down);
-        minCoordinate[i][j] = Math.min(minCoordinate[i][j], left);
-        minCoordinate[i][j] = Math.min(minCoordinate[i][j], right);
-        flag[i][j] = false;
-        return minCoordinate[i][j];
     }
+
+    private static int sum = 0;
+
+    private static int minSum = 1000000;
+
+    public static class Index {
+        public int x;
+        public int y;
+        public Index(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+/*
+    public static int bfs(Index index) {
+        LinkedList<Index> queue = new LinkedList<>();
+        queue.offer(index);
+        while(!queue.isEmpty()) {
+            Index temp = queue.poll();
+            int i = temp.x;
+            int j = temp.y;
+            sum++;
+            if(coordinate[i][j] == '*') {
+                if(sum <= minSum) {
+                    minSum = sum;
+                    sum = 0;
+                    continue;
+                }
+            }
+
+            if(i > 0 && !flag[i - 1][j]) {
+                flag[i - 1][j] = true;
+                queue.offer(new Index(i - 1, j));
+            }
+            if(i < n - 1 && !flag[i + 1][j]) {
+                flag[i + 1][j] = true;
+                queue.offer(new Index(i + 1, j));
+            }
+            if(j > 0 && !flag[i][j - 1]) {
+                flag[i][j - 1] = true;
+                queue.offer(new Index(i, j - 1));
+            }
+            if(j < m - 1 && !flag[i][j + 1]) {
+                flag[i][j + 1] = true;
+                queue.offer(new Index(i, j + 1));
+            }
+        }
+        return minSum;
+    }
+*/
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
@@ -67,12 +108,12 @@ public class Noi2727 {
             }
             coordinate = new char[n][m];
             minCoordinate = new int[n][m];
-            flag = new boolean[n][m];
             int x = 0, y = 0;
             for(int i = 0; i < n; i++) {
                 String temp = scanner.next();
                 for(int j = 0; j < m; j++) {
                     coordinate[i][j] = temp.charAt(j);
+                    minCoordinate[i][j] = Integer.MAX_VALUE;
                     if(coordinate[i][j] == '@') {
                         x = i;
                         y = j;
@@ -82,12 +123,14 @@ public class Noi2727 {
                     }
                 }
             }
-            int value = dp(x, y);
-            if(value >= MAX_VALUE) {
+            minCoordinate[x][y] = 0;
+            dfs(x, y);
+            if(ans == 0) {
                 list.add(-1);
             } else {
-                list.add(value);
+                list.add(ans);
             }
+            ans = 0;
         }
         for(Integer i : list) {
             System.out.println(i);
