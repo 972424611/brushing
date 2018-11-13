1393d5c71042ae528dabf8f92937f21303509606
aekc <972424611@qq.com>
2018-11-13 15:26:14 +0800

    complete Noi666 DFS

---
+++ src/com/aekc/noi/Noi666.java
@@ -0,0 +1,41 @@
+package com.aekc.noi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Noi666 {
+
+    private static int m;
+
+    private static int n;
+
+    private static int sum = 0;
+
+    public static void dfs(int k, int a, int b) {
+        if(a == n) {
+            sum++;
+            return;
+        }
+        for(int i = k; i <= m; i++) {
+            if(i > b - i) {
+                break;
+            }
+            dfs(i, a + 1, b - i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        int t = scanner.nextInt();
+        while(t-- != 0) {
+            m = scanner.nextInt();
+            n = scanner.nextInt();
+            dfs(0, 1, m);
+            list.add(sum);
+            sum = 0;
+        }
+        list.forEach(System.out::println);
+    }
+}
