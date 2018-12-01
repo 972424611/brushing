c1d84bfecc89a42d41fda82f1a2efc9bc0aabcd2
twilight <972424611@qq.com>
2018-12-01 21:15:57 +0800

    Complete Noi4982

--- src/com/aekc/noi/Noi4982.java
+++ src/com/aekc/noi/Noi4982.java
@@ -1,8 +1,24 @@
 package com.aekc.noi;
 
+import java.util.Scanner;
+
 /**
  * @author Twilight
  * @date 18-12-1 下午8:14
  */
 public class Noi4982 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int step1 = 1;
+        int step2 = 3;
+        for(int i = 1; i < n; i++) {
+            int k = step2 * 2 + step1;
+            step1 = step2;
+            step2 = k;
+
+        }
+        System.out.println(step2);
+    }
 }
